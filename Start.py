eebad994a069ae3502f6121b26cc86059a332394
Luis Villavicencio <MY_NAME@example.com>
2020-05-12 15:24:53 -0400

    commnet corrected

--- Start.py
+++ Start.py
@@ -30,7 +30,7 @@ while True:
     Now = datetime.now()
     date_time = Now.strftime("%Y-%m-%d %H:%M:%S")
     
-    #inyect data into big query   2
+    #Inject data into BigQuery
     query1 = (
         'INSERT `smartpi-273316.PiSensors.Sensors` (Date, Temperature, Comment,Humidity) '
         'VALUES  (DATETIME "' + str(date_time) + '",'
